7af47ec9ac7568ade38175713dbc8f3fbaf5de2b
queenley <69098210+queenley@users.noreply.github.com>
2025-03-28 15:35:10 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,1 @@
-pip install numpy==1.26.3
-pip install pandas==2.0.3
-pip install scikit-learn
-pip install matplotlib
-pip install nltk
-pip install spacy
-pip install evaluate==0.2.1
-pip install rouge
-pip install torch
-pip install transformers==4.43.3 
-pip install datasets
-pip install wandb
-pip install rouge_score
-pip install bert_score
-pip install -U gdown
-pip install python-dotenv
+pip install -r requirements.txt
